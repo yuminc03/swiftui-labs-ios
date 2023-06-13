4d97921cc158f72aed8ecc9a7b66471cba79c16b
Yumini <yuminc03@gmail.com>
2023-06-14 03:51:59 +0900

    Squashed commit of the following:
    commit d6ae65a0874116ea108fa65bb864a1025f261223
    Author: Yumini <yuminc03@gmail.com>
    Date:   Wed Jun 14 03:51:12 2023 +0900
    
        Feat: SegmentedTodos 파일 생성
    
    commit 293a3c5e6884b593249dade33000e7a5da518a43
    Author: Yumini <yuminc03@gmail.com>
    Date:   Tue Jun 13 02:02:56 2023 +0900
    
        Squashed commit of the following:
    
        commit b92ea11fdb7569cae5b99e6570956cb9d3673f43
        Author: Yumini <yuminc03@gmail.com>
        Date:   Tue Jun 13 02:01:53 2023 +0900
    
            Squashed commit of the following:
    
            commit 1769a73f12114bfdde0d652e24fac3ebe6d2f892
            Author: Yumini <yuminc03@gmail.com>
            Date:   Tue Jun 13 01:53:59 2023 +0900
    
                Refactor: AlertView RoundedRectangle -> Color로 교체
    
            commit 40b9457be60cf80af11cb054a72eefd80d9b8d0e
            Author: Yumini <yuminc03@gmail.com>
            Date:   Tue May 30 23:15:37 2023 +0900
    
                Feat: 나의 습관관리 팝업에 AppStorage 적용하기
    
        commit 5e32de979529cef15f0efeec88150d152e09e906
        Author: Yumini <yuminc03@gmail.com>
        Date:   Tue May 30 23:15:54 2023 +0900
    
            Squashed commit of the following:
    
            commit 40b9457be60cf80af11cb054a72eefd80d9b8d0e
            Author: Yumini <yuminc03@gmail.com>
            Date:   Tue May 30 23:15:37 2023 +0900
    
                Feat: 나의 습관관리 팝업에 AppStorage 적용하기
    
    commit 0f77245ad5eee786cba6912bde22d6edb614bef3
    Author: Yumini <yuminc03@gmail.com>
    Date:   Tue May 30 23:16:15 2023 +0900
    
        Squashed commit of the following:
    
        commit 5e32de979529cef15f0efeec88150d152e09e906
        Author: Yumini <yuminc03@gmail.com>
        Date:   Tue May 30 23:15:54 2023 +0900
    
            Squashed commit of the following:
    
            commit 40b9457be60cf80af11cb054a72eefd80d9b8d0e
            Author: Yumini <yuminc03@gmail.com>
            Date:   Tue May 30 23:15:37 2023 +0900
    
                Feat: 나의 습관관리 팝업에 AppStorage 적용하기

---
+++ SwiftUIPractiseExtample/Test/SegmentedTodos.swift
@@ -0,0 +1,44 @@
+//
+//  SegmentedTodos.swift
+//  SwiftUIPractiseExtample
+//
+//  Created by Yumin Chu on 2023/06/14.
+//
+
+import SwiftUI
+
+struct SegmentedTodos: View {
+    
+    @State private var selection: String = "todos"
+    private let filterOption = ["todos", "photos"]
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Picker("Picker", selection: $selection) {
+                    ForEach(filterOption.indices) { index in
+                        Text(filterOption[index])
+                        
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            .padding()
+            .navigationTitle("TODO!")
+        }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct SegmentedTodos_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedTodos()
+    }
+}
