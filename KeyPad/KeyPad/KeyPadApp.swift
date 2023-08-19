6220e233b79bde628c539bfd4d12c0817870cb38
Yumini <yuminc03@gmail.com>
2023-08-19 16:40:57 +0900

    Feat: 새로운 워크스페이스, KeyPad 프로젝트 생성

---
+++ KeyPad/KeyPad/KeyPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyPadApp.swift
+//  KeyPad
+//
+//  Created by Yumin Chu on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct KeyPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
