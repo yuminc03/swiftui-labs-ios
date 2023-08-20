ca34da32a9da6553c34396914ad5a3838f53d7f1
Yumini <yuminc03@gmail.com>
2023-08-20 17:01:04 +0900

    Feat: 설정 앱 프로젝트 생성

---
+++ Settings/Settings/SettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsApp.swift
+//  Settings
+//
+//  Created by Yumin Chu on 2023/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
