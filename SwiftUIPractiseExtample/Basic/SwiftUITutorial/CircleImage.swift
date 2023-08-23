f45484af7ca18678ac27e153f350fbec6ca36623
추유민 <ym.chu@lifesemantics.kr>
2023-08-23 09:54:19 +0900

    Feat: creating and combining views

---
+++ SwiftUIPractiseExtample/Basic/SwiftUITutorial/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  SwiftUIPractiseExtample
+//
+//  Created by Yumin Chu on 2023/08/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
