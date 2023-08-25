0ed78f6b572c2aeeb970db4663ff2f4bd99952c4
Yumini <yuminc03@gmail.com>
2023-08-26 00:13:32 +0900

    Feat: Payco project 새로 만들기

---
+++ Payco/Payco/PaycoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaycoApp.swift
+//  Payco
+//
+//  Created by Yumin Chu on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PaycoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
