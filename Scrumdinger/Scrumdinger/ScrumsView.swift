4b8bcada09b291413c305301564d0da7dbe8514a
추유민 <ym.chu@lifesemantics.kr>
2023-08-25 16:40:28 +0900

    Feat: Persisting data 작업중

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ScrumsView: View {
   @Binding var scrums: [DailyScrum]
+  @Environment(\.scenePhase) private var scenePhase
   @State private var isPresentingNewScrumView = false
+  let saveAction: () -> Void
   
   var body: some View {
     NavigationStack {
@@ -37,11 +39,14 @@ struct ScrumsView: View {
         isPresentingNewScrumView: $isPresentingNewScrumView
       )
     }
+    .onChange(of: scenePhase) { phase in
+      if phase == .inactive { saveAction() }
+    }
   }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
   static var previews: some View {
-    ScrumsView(scrums: .constant(DailyScrum.sampleData))
+    ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
   }
 }
