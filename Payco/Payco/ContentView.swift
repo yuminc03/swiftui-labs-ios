969ac8f15cddda84a7a62159d28131294407419f
Yumini <yuminc03@gmail.com>
2023-08-27 00:00:37 +0900

    Feat: 네 번째 메뉴 작업중

--- Payco/Payco/ContentView.swift
+++ Payco/Payco/ContentView.swift
@@ -12,6 +12,7 @@ import ComposableArchitecture
 struct ContentCore: Reducer {
   struct State: Equatable {
     var selectedIndex = 0
+    let menu1 = PointMenuItem.dummy
   }
   
   enum Action {
@@ -29,6 +30,25 @@ struct ContentCore: Reducer {
 }
 
 struct ContentView: View {
+  private let columns = [
+    GridItem(.flexible(), spacing: 20, alignment: .center),
+    GridItem(.flexible(), spacing: 20, alignment: .center),
+    GridItem(.flexible(), spacing: 20, alignment: .center),
+    GridItem(.flexible(), spacing: 20, alignment: .center)
+  ]
+  private let pointpaymentBenefitColumns = [
+    GridItem(.flexible(), spacing: 10, alignment: .center)
+  ]
+  private let store: StoreOf<ContentCore>
+  @ObservedObject private var viewStore: ViewStoreOf<ContentCore>
+  
+  init() {
+    self.store = .init(initialState: .init()) {
+      ContentCore()
+    }
+    self.viewStore = ViewStore(store, observe: { $0 })
+  }
+  
   var body: some View {
     NavigationView {
       List {
@@ -41,7 +61,51 @@ struct ContentView: View {
             TopRightButton(imageName: "person")
           }
         }
+        .listRowSeparator(.hidden)
+
+        Section {
+          section1
+        }
+        .listRowSeparator(.hidden)
         
+        Section {
+          section2
+        }
+        .listRowSeparator(.hidden)
+        
+        Section {
+          section1
+        }
+        .listRowSeparator(.hidden)
+        
+        Section {
+          section4
+        }
+        .listRowSeparator(.hidden)
+        
+        Section {
+          
+        }
+        .listRowSeparator(.hidden)
+        
+        Section {
+          
+        }
+        .listRowSeparator(.hidden)
+        
+        Section {
+          
+        }
+        .listRowSeparator(.hidden)
+        
+        Section {
+          
+        }
+        .listRowSeparator(.hidden)
+        
+        Section {
+          
+        }
         .listRowSeparator(.hidden)
       }
       .listStyle(.plain)
@@ -63,5 +127,47 @@ extension ContentView {
       .bold()
   }
   
+  var section1: some View {
+    RoundedRectangle(cornerRadius: 20)
+      .frame(height: 150)
+      .foregroundColor(.gray)
+  }
   
+  var section2: some View {
+    LazyVGrid(columns: columns, spacing: 20) {
+      ForEach(viewStore.menu1) { item in
+        VStack(spacing: 10) {
+          Image(systemName: item.imageName)
+            .font(.largeTitle)
+            .foregroundColor(item.iconColor)
+          Text(item.title)
+            .font(.caption)
+        }
+      }
+    }
+  }
+  
+  var section4: some View {
+    RoundedRectangle(cornerRadius: 20)
+      .foregroundColor(Color("gray_EAEAEA"))
+      .frame(height: 500)
+      .overlay {
+        VStack(spacing: 20) {
+          HStack {
+            Text("8월 포인트 결제 혜택")
+              .font(.title3)
+              .bold()
+            Spacer()
+          }
+          .padding(.leading, 20)
+          
+        }
+      }
+  }
+  
+  var pointpaymentBenefitMenu: some View {
+    LazyHGrid(rows: pointpaymentBenefitColumns, spacing: 20) {
+      
+    }
+  }
 }
