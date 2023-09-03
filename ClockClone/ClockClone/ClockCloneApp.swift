b2336cb8f5e7184ef96a90d04fa2bb314b45e431
Yumini <yuminc03@gmail.com>
2023-09-03 17:35:25 +0900

    Feat: ClockClone Project 생성

---
+++ ClockClone/ClockClone/ClockCloneApp.swift
@@ -0,0 +1,18 @@
+//
+//  ClockCloneApp.swift
+//  ClockClone
+//
+//  Created by Yumin Chu on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct ClockCloneApp: App {
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
