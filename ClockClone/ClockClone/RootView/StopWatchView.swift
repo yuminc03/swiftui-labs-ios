4df254215f38693d1626fdf92cda12f1e4d58afe
Yumini <yuminc03@gmail.com>
2023-09-04 23:47:54 +0900

    Refactor: 스톱워치 view 수정

--- ClockClone/ClockClone/RootView/StopWatchView.swift
+++ ClockClone/ClockClone/RootView/StopWatchView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct StopWatchView: View {
  
   var body: some View {
-    ZStack {
-      Color.black
-        .ignoresSafeArea()
-      VStack(spacing: 0) {
+    VStack(spacing: 0) {
+      ZStack {
         VStack(alignment: .center, spacing: 20) {
           tabView
+          Spacer()
+        }
+        VStack(spacing: 0) {
+          Spacer()
           HStack(spacing: 0) {
             stopWatchButton(title: "랩", type: .darkGray)
             Spacer()
@@ -23,22 +25,24 @@ struct StopWatchView: View {
           }
         }
         .padding(.bottom, 10)
-        List {
-          Divider()
-            .background(.gray)
-            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-        }
-        .listStyle(.plain)
-        .frame(height: UIScreen.main.bounds.height / 3)
       }
-      .padding(.horizontal, 20)
+      List {
+        Divider()
+          .background(.gray)
+          .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+          .listRowSeparator(.hidden)
+      }
+      .listStyle(.plain)
+      .frame(height: UIScreen.main.bounds.height / 3)
     }
+    .padding(.horizontal, 20)
   }
 }
 
 struct StopWatchView_Previews: PreviewProvider {
   static var previews: some View {
     StopWatchView()
+      .previewLayout(.sizeThatFits)
   }
 }
 
@@ -62,6 +66,7 @@ extension StopWatchView {
       .foregroundColor(type.titleColor)
       .padding(30)
       .clipShape(Circle())
+      .frame(width: 90, height: 90)
       .background {
         ZStack(alignment: .center) {
           Circle()
