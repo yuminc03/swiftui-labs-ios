fc896b139c108e29d82caeb86e1cc242feeecaba
추유민 <ym.chu@lifesemantics.kr>
2023-09-05 14:18:20 +0900

    Feat: 랩 버튼 이벤트 작업중

--- ClockClone/ClockClone/RootView/StopWatchView.swift
+++ ClockClone/ClockClone/RootView/StopWatchView.swift
@@ -18,30 +18,48 @@ struct StopWatchCore: Reducer {
     var isTapStartButton = false
     var isTapStopButton = false
     var isTapReStartButton = false
-    var timerMinutes = 0
-    var timerSeconds = 0
-    var timerMilliSeconds = 0
+    var timerMinute = 0
+    var timerSecond = 0
+    var timerMilliSecond = 0
+    var savedMilliSeconds = [Int]()
+    var raps = [String]()
+    var rapMilliSecond = 0
+    var rapSecond = 0
+    var rapMinute = 0
+    var rapMilliText = "00"
+    var rapSecondText = "00"
+    var rapMinuteText = "00"
   }
   
   enum Action {
     case didTapRapButton
     case didTapToggleStartButton
+    case rapTimerStart
     case didTapResetButton
     case didTapReStartButton
     case timerTicked
-    case onDisappear
+    case stopWatchAction
+    case rapAction
+    case rapTimerTicked
+    case didCancelStopWatch
+    case didCancelRap
   }
   
   private enum CancelID {
-    case timer
+    case stopWatch
+    case rap
   }
   
-  @Dependency(\.continuousClock) var clock
+  @Dependency(\.continuousClock) var stopWatchClock
+  @Dependency(\.continuousClock) var rapClock
   
   func reduce(into state: inout State, action: Action) -> Effect<Action> {
     switch action {
     case .didTapRapButton:
-      return .none
+      guard state.isStartButton == false else { return .none }
+      return .run { send in
+        await send(.rapTimerStart)
+      }
       
     case .didTapToggleStartButton:
       if state.isStartButton {
@@ -52,13 +70,19 @@ struct StopWatchCore: Reducer {
         state.isTapReStartButton = false
       }
       state.isStartButton.toggle()
-      return .run { [isStartButton = state.isStartButton] send in
-        guard isStartButton == false else { return }
-        for await _ in clock.timer(interval: .seconds(0.01)) {
-          await send(.timerTicked)
-        }
+      return .run { send in
+        await send(.stopWatchAction)
       }
-      .cancellable(id: CancelID.timer, cancelInFlight: true)
+      
+    case .rapTimerStart:
+      guard state.isStartButton == false else { return .none }
+      state.raps.append(
+        "\(state.rapMinuteText):\(state.rapSecondText).\(state.rapMilliText)"
+      )
+      state.savedMilliSeconds.append(state.rapMilliSecond)
+      return .run { send in
+        await send(.rapAction)
+      }
       
     case .didTapResetButton:
       state.minuteText = "00"
@@ -68,45 +92,94 @@ struct StopWatchCore: Reducer {
       state.isTapStartButton = false
       state.isTapStopButton = false
       state.isTapReStartButton = false
-      state.timerMilliSeconds = 0
-      state.timerSeconds = 0
-      state.timerMinutes = 0
-      return .cancel(id: CancelID.timer)
+      state.timerMilliSecond = 0
+      state.timerSecond = 0
+      state.timerMinute = 0
+      state.raps = []
+      state.savedMilliSeconds = []
+      return .cancel(id: CancelID.stopWatch)
       
     case .didTapReStartButton:
       state.isTapReStartButton = true
       return .none
       
     case .timerTicked:
-      state.timerMilliSeconds += 1
-      if Int(state.timerMilliSeconds % 100) < 10 {
-        state.millisecondText = "0\(Int(state.timerMilliSeconds % 100))"
+      state.timerMilliSecond += 1
+      if Int(state.timerMilliSecond % 100) < 10 {
+        state.millisecondText = "0\(Int(state.timerMilliSecond % 100))"
       } else {
-        state.millisecondText = "\(Int(state.timerMilliSeconds % 100))"
+        state.millisecondText = "\(Int(state.timerMilliSecond % 100))"
       }
       
-      guard state.timerMilliSeconds / 100 > 0 else { return .none }
+      guard state.timerMilliSecond / 100 > 0 else { return .none }
       
-      state.timerSeconds = state.timerMilliSeconds / 100
-      if Int(state.timerSeconds % 60) < 10 {
-        state.secondText = "0\(Int(state.timerSeconds % 60))"
+      state.timerSecond = state.timerMilliSecond / 100
+      if Int(state.timerSecond % 60) < 10 {
+        state.secondText = "0\(Int(state.timerSecond % 60))"
       } else {
-        state.secondText = "\(Int(state.timerSeconds % 60))"
+        state.secondText = "\(Int(state.timerSecond % 60))"
       }
 
-      guard state.timerSeconds / 60 > 0 else { return .none }
+      guard state.timerSecond / 60 > 0 else { return .none }
 
-      state.timerMinutes = state.timerSeconds / 60
-      if Int(state.timerMinutes % 60) < 10 {
-        state.minuteText = "0\(Int(state.timerMinutes % 60))"
+      state.timerMinute = state.timerSecond / 60
+      if Int(state.timerMinute % 60) < 10 {
+        state.minuteText = "0\(Int(state.timerMinute % 60))"
       } else {
-        state.minuteText = "\(Int(state.timerMinutes % 60))"
+        state.minuteText = "\(Int(state.timerMinute % 60))"
       }
-
       return .none
       
-    case .onDisappear:
-      return .cancel(id: CancelID.timer)
+    case .stopWatchAction:
+      return .run { [isStartButton = state.isStartButton] send in
+        guard isStartButton == false else { return }
+        for await _ in stopWatchClock.timer(interval: .seconds(0.01)) {
+          await send(.timerTicked)
+        }
+      }
+      .cancellable(id: CancelID.stopWatch, cancelInFlight: true)
+      
+    case .rapAction:
+      return .run { [state = state] send in
+        guard state.isStartButton == false else { return }
+        for await _ in rapClock.timer(interval: .seconds(0.01)) {
+          await send(.rapTimerTicked)
+        }
+      }
+      .cancellable(id: CancelID.rap, cancelInFlight: true)
+      
+    case .rapTimerTicked:
+      state.rapMilliSecond += 1
+      if Int(state.rapMilliSecond % 100) < 10 {
+        state.rapMilliText = "0\(Int(state.rapMilliSecond % 100))"
+      } else {
+        state.rapMilliText = "\(Int(state.rapMilliSecond % 100))"
+      }
+      
+      guard state.rapMilliSecond / 100 > 0 else { return .none }
+      
+      state.rapSecond = state.timerMilliSecond / 100
+      if Int(state.rapSecond % 60) < 10 {
+        state.rapSecondText = "0\(Int(state.rapSecond % 60))"
+      } else {
+        state.rapSecondText = "\(Int(state.rapSecond % 60))"
+      }
+
+      guard state.rapSecond / 60 > 0 else { return .none }
+
+      state.rapMinute = state.rapSecond / 60
+      if Int(state.rapMinute % 60) < 10 {
+        state.rapMinuteText = "0\(Int(state.rapMinute % 60))"
+      } else {
+        state.rapMinuteText = "\(Int(state.rapMinute % 60))"
+      }
+      return .none
+      
+    case .didCancelStopWatch:
+      return .cancel(id: CancelID.stopWatch)
+      
+    case .didCancelRap:
+      return .cancel(id: CancelID.rap)
     }
   }
 }
@@ -158,6 +231,7 @@ struct StopWatchView: View {
             if viewStore.isStartButton {
               stopWatchButton(title: "시작", type: .green) {
                 store.send(.didTapToggleStartButton)
+                store.send(.rapTimerStart)
               }
             } else {
               stopWatchButton(title: "중단", type: .red) {
@@ -168,18 +242,29 @@ struct StopWatchView: View {
         }
         .padding(.bottom, 10)
       }
-      List {
+      if viewStore.raps.isEmpty {
         Divider()
           .background(.gray)
-          .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-          .listRowSeparator(.hidden)
+        Rectangle()
+          .fill(.black)
+          .frame(height: UIScreen.main.bounds.height / 3)
+      } else {
+        List {
+          ForEach(0 ..< viewStore.raps.count) { index in
+            StopWatchRow(
+              labTime: LabTimeItem(id: index + 1, savedTime: viewStore.raps[index]),
+              colorType: .white
+            )
+          }
+        }
+        .listStyle(.plain)
+        .frame(height: UIScreen.main.bounds.height / 3)
       }
-      .listStyle(.plain)
-      .frame(height: UIScreen.main.bounds.height / 3)
     }
     .padding(.horizontal, 20)
     .onDisappear {
-      store.send(.onDisappear)
+      store.send(.didCancelStopWatch)
+      store.send(.didCancelRap)
     }
   }
 }
