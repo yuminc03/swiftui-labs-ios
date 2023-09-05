3a5ca9404df705a7e4d7838ad115e80fa401d7dd
추유민 <ym.chu@lifesemantics.kr>
2023-09-05 12:59:03 +0900

    Feat: 스톱워치 기능 추가, UI 수정

--- ClockClone/ClockClone/RootView/StopWatchView.swift
+++ ClockClone/ClockClone/RootView/StopWatchView.swift
@@ -11,21 +11,29 @@ import ComposableArchitecture
 
 struct StopWatchCore: Reducer {
   struct State: Equatable {
-    var hourText = "00"
     var minuteText = "00"
-    var secondsText = "00"
+    var secondText = "00"
+    var millisecondText = "00"
     var isStartButton = true
     var isTapStartButton = false
     var isTapStopButton = false
     var isTapReStartButton = false
+    var timerMinutes = 0
+    var timerSeconds = 0
+    var timerMilliSeconds = 0
   }
   
   enum Action {
     case didTapRapButton
-    case didTapStartButton
+    case didTapToggleStartButton
     case didTapResetButton
-    case didTapStopButton
     case didTapReStartButton
+    case timerTicked
+    case onDisappear
+  }
+  
+  private enum CancelID {
+    case timer
   }
   
   @Dependency(\.continuousClock) var clock
@@ -35,31 +43,70 @@ struct StopWatchCore: Reducer {
     case .didTapRapButton:
       return .none
       
-    case .didTapStartButton:
-      state.isTapStartButton = true
-      state.isTapReStartButton = true
+    case .didTapToggleStartButton:
+      if state.isStartButton {
+        state.isTapStartButton = true
+        state.isTapReStartButton = true
+      } else {
+        state.isTapStopButton = true
+        state.isTapReStartButton = false
+      }
       state.isStartButton.toggle()
-      return .none
+      return .run { [isStartButton = state.isStartButton] send in
+        guard isStartButton == false else { return }
+        for await _ in clock.timer(interval: .seconds(0.01)) {
+          await send(.timerTicked)
+        }
+      }
+      .cancellable(id: CancelID.timer, cancelInFlight: true)
       
     case .didTapResetButton:
-      state.hourText = "00"
       state.minuteText = "00"
-      state.secondsText = "00"
+      state.secondText = "00"
+      state.millisecondText = "00"
       state.isStartButton = true
       state.isTapStartButton = false
       state.isTapStopButton = false
       state.isTapReStartButton = false
-      return .none
-      
-    case .didTapStopButton:
-      state.isTapStopButton = true
-      state.isStartButton.toggle()
-      state.isTapReStartButton = false
-      return .none
+      state.timerMilliSeconds = 0
+      state.timerSeconds = 0
+      state.timerMinutes = 0
+      return .cancel(id: CancelID.timer)
       
     case .didTapReStartButton:
       state.isTapReStartButton = true
       return .none
+      
+    case .timerTicked:
+      state.timerMilliSeconds += 1
+      if Int(state.timerMilliSeconds % 100) < 10 {
+        state.millisecondText = "0\(Int(state.timerMilliSeconds % 100))"
+      } else {
+        state.millisecondText = "\(Int(state.timerMilliSeconds % 100))"
+      }
+      
+      guard state.timerMilliSeconds / 100 > 0 else { return .none }
+      
+      state.timerSeconds = state.timerMilliSeconds / 100
+      if Int(state.timerSeconds % 60) < 10 {
+        state.secondText = "0\(Int(state.timerSeconds % 60))"
+      } else {
+        state.secondText = "\(Int(state.timerSeconds % 60))"
+      }
+
+      guard state.timerSeconds / 60 > 0 else { return .none }
+
+      state.timerMinutes = state.timerSeconds / 60
+      if Int(state.timerMinutes % 60) < 10 {
+        state.minuteText = "0\(Int(state.timerMinutes % 60))"
+      } else {
+        state.minuteText = "\(Int(state.timerMinutes % 60))"
+      }
+
+      return .none
+      
+    case .onDisappear:
+      return .cancel(id: CancelID.timer)
     }
   }
 }
@@ -110,11 +157,11 @@ struct StopWatchView: View {
             Spacer()
             if viewStore.isStartButton {
               stopWatchButton(title: "시작", type: .green) {
-                store.send(.didTapStartButton)
+                store.send(.didTapToggleStartButton)
               }
             } else {
               stopWatchButton(title: "중단", type: .red) {
-                store.send(.didTapStopButton)
+                store.send(.didTapToggleStartButton)
               }
             }
           }
@@ -131,6 +178,9 @@ struct StopWatchView: View {
       .frame(height: UIScreen.main.bounds.height / 3)
     }
     .padding(.horizontal, 20)
+    .onDisappear {
+      store.send(.onDisappear)
+    }
   }
 }
 
@@ -145,12 +195,8 @@ extension StopWatchView {
   
   var tabView: some View {
     TabView {
-      Text("\(viewStore.hourText):\(viewStore.minuteText):\(viewStore.secondsText)")
-        .fontWeight(.thin)
-        .font(.system(size: 80))
-        .minimumScaleFactor(0.01)
-        .frame(maxWidth: .infinity)
-      Text("Clock")
+      stopWatchView
+      clockView
     }
     .foregroundColor(.white)
     .tabViewStyle(.page)
@@ -181,5 +227,25 @@ extension StopWatchView {
       .onTapGesture {
         action()
       }
+  }
+  
+  var stopWatchView: some View {
+    HStack(alignment: .center, spacing: 0) {
+      Text(viewStore.minuteText)
+        .frame(width: 110)
+      Text(":")
+      Text(viewStore.secondText)
+        .frame(width: 110)
+      Text(".")
+      Text(viewStore.millisecondText)
+        .frame(width: 110)
+    }
+    .fontWeight(.thin)
+    .font(.system(size: UIScreen.main.bounds.width / 5))
+    .frame(maxWidth: .infinity)
+  }
+  
+  var clockView: some View {
+    Text("Clock")
   }
 }
