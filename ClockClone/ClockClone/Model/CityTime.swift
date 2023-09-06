79e55225be3c7f35f48c7397a4350ebaa522a416
추유민 <ym.chu@lifesemantics.kr>
2023-09-06 09:21:54 +0900

    Fix: 랜덤 날짜 잘 안나오는 문제 수정

--- ClockClone/ClockClone/Model/CityTime.swift
+++ ClockClone/ClockClone/Model/CityTime.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 enum CityTime: String, Equatable, CaseIterable {
-  case spanish = "es_ES"
-  case thai = "th_TH"
-  case korean = "ko_KR"
-  case italian = "it_IT"
-  case portuguese = "pt_MZ"
-  case english = "en_US_POSIX"
-  case tonga = "to_TO"
-  case icelandic = "is_IS"
-  case danish = "da_DK"
-  case germany = "de_DE"
+  case abidjan = "Africa/Abidjan"
+  case algiers = "Africa/Algiers"
+  case korean = "Asia/Seoul"
+  case anguilla = "America/Anguilla"
+  case belize = "America/Belize"
+  case english = "GMT"
+  case casey = "Antarctica/Casey"
+  case atyrau = "Asia/Atyrau"
+  case gaza = "Asia/Gaza"
+  case truk = "Pacific/Truk"
   
   static var randomTime: String {
     return CityTime.dummy.randomElement() ?? DateFormat.convertTimeToString(id: CityTime.korean.rawValue)
@@ -31,7 +31,8 @@ class DateFormat {
   static func convertTimeToString(id: String) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "a h:mm"
-    dateFormatter.locale = Locale(identifier: id)
+    dateFormatter.locale = Locale(identifier: Locale.current.identifier)
+    dateFormatter.timeZone = TimeZone(identifier: id)
     return dateFormatter.string(from: Date())
   }
 }
