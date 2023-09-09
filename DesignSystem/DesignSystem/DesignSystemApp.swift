7eebebb55b8adb9e496793b5c11551f87baacef4
Yumini <yuminc03@gmail.com>
2023-09-10 00:19:13 +0900

    Feat: DesignSystem 폴더 추가, 필요없는 scheme 삭제

---
+++ DesignSystem/DesignSystem/DesignSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemApp.swift
+//  DesignSystem
+//
+//  Created by Yumin Chu on 2023/09/10.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
