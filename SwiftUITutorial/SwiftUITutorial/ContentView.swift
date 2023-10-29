de46ab72f3d3ff1dec50831e762c1f1f1438d67d
Yumin Chu <yuminc03@gmail.com>
2023-10-29 20:58:28 +0900

    Feat: SwiftUITutorial Project

---
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Yumin Chu on 2023/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Yumin Chu on 2023/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
