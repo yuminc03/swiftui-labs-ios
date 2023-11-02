eb528f704000781e593a94850d5cd1906ca62c68
Yumin Chu <ym.chu@lifesemantics.kr>
2023-11-02 10:03:54 +0900

    Feat: Search 프로젝트 생성

---
+++ Search/Search/SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchApp.swift
+//  Search
+//
+//  Created by LS-NOTE-00106 on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Search/Search/WeatherClient.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherClient.swift
+//  Search
+//
+//  Created by LS-NOTE-00106 on 2023/11/02.
+//
+
+import Foundation
