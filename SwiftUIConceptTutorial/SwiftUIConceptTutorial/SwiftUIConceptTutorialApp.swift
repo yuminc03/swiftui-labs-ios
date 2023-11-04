96ae6d41c92f6e0b4eab1fcfa1382bd5c077c018
Yumin Chu <yuminc03@gmail.com>
2023-11-05 02:00:05 +0900

    Feat: SwiftUIConceptTutorial 분리를 위한 프로젝트

---
+++ SwiftUIConceptTutorial/SwiftUIConceptTutorial/SwiftUIConceptTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConceptTutorialApp.swift
+//  SwiftUIConceptTutorial
+//
+//  Created by Yumin Chu on 2023/11/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConceptTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
