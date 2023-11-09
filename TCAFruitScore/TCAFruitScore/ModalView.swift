bb9bf80adf5ab0c60fdae9057c1c6291d0aea19d
Yumin Chu <yuminc03@gmail.com>
2023-11-09 22:56:39 +0900

    Feat: ModalView Reducer

--- TCAFruitScore/TCAFruitScore/ModalView.swift
+++ TCAFruitScore/TCAFruitScore/ModalView.swift
@@ -1,15 +1,57 @@
 import SwiftUI
 
+import ComposableArchitecture
+
+struct ModalCore: Reducer {
+  struct State: Equatable {
+    
+  }
+  
+  enum Action {
+    case didTapBackButton
+    case delegate(Delegate)
+    
+    enum Delegate: Equatable {
+      case increase
+    }
+  }
+  
+  @Dependency(\.dismiss) var dismiss
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .didTapBackButton:
+        return .run { send in
+          await send(.delegate(.increase))
+          await dismiss()
+        }
+        
+      case .delegate:
+        return .none
+      }
+    }
+  }
+}
+
 struct ModalView: View {
+  private let store: StoreOf<ModalCore>
+  
+  init(store: StoreOf<ModalCore>) {
+    self.store = store
+  }
+  
   var body: some View {
     blueButton(title: "Back") {
-      
+      store.send(.didTapBackButton)
     }
   }
 }
 
 #Preview {
-  ModalView()
+  ModalView(store: .init(initialState: ModalCore.State()) {
+    ModalCore()
+  })
 }
 
 extension ModalView {
