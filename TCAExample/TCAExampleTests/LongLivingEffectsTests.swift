0a2d58eb7e0545fcc9ebd657938a31afbe1dc983
Yumin Chu <yuminc03@gmail.com>
2023-11-23 22:20:21 +0900

    Feat: LongLivingEffectsTests

---
+++ TCAExample/TCAExampleTests/LongLivingEffectsTests.swift
@@ -0,0 +1,30 @@
+//
+//  LongLivingEffectsTests.swift
+//  TCAExampleTests
+//
+//  Created by Yumin Chu on 2023/11/23.
+//
+
+import XCTest
+
+import ComposableArchitecture
+
+@testable import TCAExample
+
+final class LongLivingEffectsTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    continueAfterFailure = false
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  
+}
