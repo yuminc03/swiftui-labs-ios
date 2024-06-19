a5f7ab855972f1e5e53419dd4a658abf2a876588
Yumin Chu <yuminc03@gmail.com>
2024-06-19 16:21:35 +0900

    Feat: TCACoordinator 테스트를 위한 새 프로젝트 생성

---
+++ TCACoordinatorExample/TCACoordinatorExample/App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCACoordinatorExample
+//
+//  Created by LS-NOTE-00106 on 6/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ TCACoordinatorExample/TCACoordinatorExample/App/TCACoordinatorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACoordinatorExampleApp.swift
+//  TCACoordinatorExample
+//
+//  Created by LS-NOTE-00106 on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TCACoordinatorExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
