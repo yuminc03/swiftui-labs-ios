bab52d44cb1998ea6fb49cf2e405e771c2692ea6
Yumin Chu <yuminc03@gmail.com>
2024-06-20 14:23:46 +0900

    Feat: MainTab에 들어갈 State, Action 추가

--- TCACoordinatorExample/TCACoordinatorExample/Scene/MainFeature/Coordinator/MainTabCoordinator.swift
+++ TCACoordinatorExample/TCACoordinatorExample/Scene/MainFeature/Coordinator/MainTabCoordinator.swift
@@ -5,7 +5,6 @@
 //  Created by LS-NOTE-00106 on 6/20/24.
 //
 
-import Foundation
 import SwiftUI
 
 import ComposableArchitecture
@@ -14,14 +13,27 @@ import TCACoordinators
 @Reducer
 struct MainTabCoordinator {
   struct State: Equatable {
-    
+    var home: HomeCoordinator.State
+    var clinicList: ClinicHistoryCoordinator.State
+    var prescription: PrescriptionReservationCoordinator.State
   }
   
   enum Action: Equatable {
-    
+    case home(HomeCoordinator.Action)
+    case clinicList(ClinicHistoryCoordinator.Action)
+    case prescription(PrescriptionReservationCoordinator.Action)
   }
   
   var body: some ReducerOf<Self> {
+    Scope(state: \.home, action: /Action.home) {
+      HomeCoordinator()
+    }
+    Scope(state: \.clinicList, action: /Action.clinicList) {
+      ClinicHistoryCoordinator()
+    }
+    Scope(state: \.prescription, action: /Action.prescription) {
+      PrescriptionReservationCoordinator()
+    }
     Reduce { state, action in
       switch action {
       default: break
