9dde601dc44bd127e7a2583c69d773953f6f527f
Yumin Chu <yuminc03@gmail.com>
2024-06-20 14:41:47 +0900

    Feat: TabBar에 사용할 Tab enum

---
+++ TCACoordinatorExample/TCACoordinatorExample/DesignSystem/Component/TabBar/Tab.swift
@@ -0,0 +1,38 @@
+//
+//  Tab.swift
+//  TCACoordinatorExample
+//
+//  Created by LS-NOTE-00106 on 6/20/24.
+//
+
+import Foundation
+
+enum Tab: Hashable, CaseIterable {
+  case home
+  case clinicList
+  case prescriptionList
+  
+  var title: String {
+    switch self {
+    case .home: return "홈"
+    case .clinicList: return "진료"
+    case .prescriptionList: return "조제"
+    }
+  }
+  
+  var icon: String {
+    switch self {
+    case .home: return "house"
+    case .clinicList: return "stethoscope.circle"
+    case .prescriptionList: return "pill"
+    }
+  }
+  
+  var selectedIcon: String {
+    switch self {
+    case .home: return "house.fill"
+    case .clinicList: return "stethoscope.circle.fill"
+    case .prescriptionList: return "pill.fill"
+    }
+  }
+}
