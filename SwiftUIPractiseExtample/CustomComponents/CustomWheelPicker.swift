8d272e4c163beccb716d2d720d3d6f05c97b3574
Yumin Chu <yuminc03@gmail.com>
2025-02-11 23:01:47 +0900

    Feat: Custom Wheel Picker를 붙일 화면 생성

---
+++ SwiftUIPractiseExtample/CustomComponents/CustomWheelPicker.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct CustomWheelPicker: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+private extension CustomWheelPicker {
+  
+}
+
+#Preview {
+  CustomWheelPicker()
+}
--- SwiftUIPractiseExtample/RootView.swift
+++ SwiftUIPractiseExtample/RootView.swift
@@ -1,60 +1,63 @@
-//
-//  RootView.swift
-//  SwiftUIPractiseExtample
-//
-//  Created by Yumin Chu on 2023/08/14.
-//
-
 import SwiftUI
 
 struct RootView: View {
-    
-    var body: some View {
-        NavigationView {
-            Form {
-                Section("🫐 Getting started") {
-                    NavigationLink(destination: TextFontExampleView()) {
-                        Text("Text and Font")
-                    }
-                    NavigationLink(destination: SafeAreaRegionsExampleView()) {
-                        Text("SafeArea Regions")
-                    }
-                    NavigationLink(destination: TextFieldExampleView()) {
-                        Text("Input Name in TextField")
-                    }
-                    NavigationLink(destination: ListBootcampView()) {
-                        Text("List")
-                    }
-                }
-                Section("🍎 iOS Team Challenge") {
-                    NavigationLink(destination: RectanglePictureView()) {
-                        Text("Rectangle Picture")
-                    }
-                    NavigationLink(destination: ManagementProfileView()) {
-                        Text("Management Profile")
-                    }
-                    NavigationLink(destination: RoutinePopupExampleView()) {
-                        Text("Routine Popup")
-                    }
-                    NavigationLink(destination: SegmentedTodosView()) {
-                        Text("Todos")
-                    }
-                }
-              Section("🍒 UIViewRepresentable Prectise") {
-                NavigationLink {
-                  VideoView()
-                } label: {
-                  Text("Represented AVPlayer")
-                }
-              }
-            }
-            .navigationTitle("SwiftUI Practise")
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section("🫐 Getting started") {
+          NavigationLink(destination: TextFontExampleView()) {
+            Text("Text and Font")
+          }
+          NavigationLink(destination: SafeAreaRegionsExampleView()) {
+            Text("SafeArea Regions")
+          }
+          NavigationLink(destination: TextFieldExampleView()) {
+            Text("Input Name in TextField")
+          }
+          NavigationLink(destination: ListBootcampView()) {
+            Text("List")
+          }
         }
+        
+        Section("🍎 iOS Team Challenge") {
+          NavigationLink(destination: RectanglePictureView()) {
+            Text("Rectangle Picture")
+          }
+          NavigationLink(destination: ManagementProfileView()) {
+            Text("Management Profile")
+          }
+          NavigationLink(destination: RoutinePopupExampleView()) {
+            Text("Routine Popup")
+          }
+          NavigationLink(destination: SegmentedTodosView()) {
+            Text("Todos")
+          }
+        }
+        
+        Section("🍒 UIViewRepresentable Prectise") {
+          NavigationLink {
+            VideoView()
+          } label: {
+            Text("Represented AVPlayer")
+          }
+        }
+        
+        Section("🍣 Custom Components") {
+          NavigationLink {
+            CustomWheelPicker()
+          } label: {
+            Text("Custom Wheel Picker")
+          }
+        }
+      }
+      .navigationTitle("SwiftUI Practise")
     }
+  }
 }
 
 struct RootView_Previews: PreviewProvider {
-    static var previews: some View {
-        RootView()
-    }
+  static var previews: some View {
+    RootView()
+  }
 }
